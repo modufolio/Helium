99fc7e82655eca38adbab757c10f47ff761ddcbf
Maarten Thiebou <thiebou@gmail.com>
2022-08-27 11:01:30 +0200

    Load composer autoloader if present

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once __DIR__ . '/app/helpers.php';
-require_once __DIR__ . '/app/controllers/Controller.php';
-require_once __DIR__ . '/app/App.php';
-require_once __DIR__ . '/app/Response.php';
+if (is_file($autoloader = __DIR__ . '/vendor/autoload.php')) {
+    include $autoloader;
+} else {
+    require_once __DIR__ . '/app/controllers/Controller.php';
+    require_once __DIR__ . '/app/App.php';
+    require_once __DIR__ . '/app/Response.php';
+}
