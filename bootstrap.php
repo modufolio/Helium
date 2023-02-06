7875b0ed583cea194ae0d7227c614aa066887987
Maarten Thiebou <thiebou@gmail.com>
2023-02-06 18:29:04 +0100

    move helper file

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once __DIR__ . '/app/helpers.php';
+require_once __DIR__ . '/helpers.php';
+require_once __DIR__ . '/app/Controllers/Controller.php';
+require_once __DIR__ . '/app/App.php';
+require_once __DIR__ . '/app/Response.php';
+
 if (is_file($autoloader = __DIR__ . '/vendor/autoload.php')) {
     include $autoloader;
-} else {
-    require_once __DIR__ . '/app/controllers/Controller.php';
-    require_once __DIR__ . '/app/App.php';
-    require_once __DIR__ . '/app/Response.php';
 }
